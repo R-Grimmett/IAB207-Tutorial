3cf4e4d04b8a66aee0dad56f3c8a24d5fe841b33
R-Grimmett <grimmett.rachael@gmail.com>
2025-05-17 21:22:38 +1000

    Tutorial 10 Task 1

--- travel/views.py
+++ travel/views.py
@@ -1,8 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for
+from werkzeug.utils import redirect
+
+from . import db
+
+from .models import Destination
 
 mainbp = Blueprint('main', __name__)
 
 
 @mainbp.route('/')
 def index():
-    return render_template('index.html')
+    destinations = db.session.scalars(db.select(Destination)).all()
+    return render_template('index.html', destinations=destinations)
+
+
+@mainbp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        destinations = db.session.scalars(db.select(Destination).where(Destination.description.like(query)))
+        return render_template('index.html', destinations=destinations)
+    else:
+        return redirect(url_for('main.index'))
